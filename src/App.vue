f9d8f7c4c3824ecd632750a41ca21df4c832adac
Flat-Pixels <pixelsplats@gmail.com>
2019-03-27 13:43:53 -0700

    feat: All basic features of todo integrated
    - Add tasks group
    - Delete tasks group
    - Add tasks to a tasks group
    - Mark a task as completed or uncompleted
    - Delete a task

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button
+      class="JS-add-tasks-group btn btn--light"
+      @click="addTasksGroup()">
+      add tasks group
+    </button>
+
+    <div class="container">
+
+      <div 
+        v-for="tasksGroup in tasksGroups">
+
+        <tasks-group
+          v-if="!tasksGroup.deleted"
+          @delete="deleteTasksGroup(tasksGroup)">
+        </tasks-group>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TasksGroup from './components/TasksGroup';
+import Vue from 'vue';
 
 export default {
   name: 'app',
+  data : function(){
+    return {
+      tasksGroups: []
+    }
+  },
+  methods: {
+    /**
+     * Add a new tasksGroup
+     * Set the property {deleted} to false
+     */
+    addTasksGroup: function(){
+      this.tasksGroups.push({deleted: false});
+    },
+
+    /**
+     * Delete a specific tasksGroup
+     * @param  {Object} tasksGroup The tasksGroup to delete
+     * @return {Void}
+     */
+    deleteTasksGroup: function(tasksGroup){
+      tasksGroup.deleted = true;
+    },
+  },
   components: {
-    HelloWorld
+    TasksGroup
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  padding: 0;
+  margin: 0;
+
+  background-color: #e5e5f5;
+
+  font-family: "Source sans pro", sans-serif;
+  font-size: 17px;
+}
+
+* { box-sizing: border-box; }
+
+.container {
+  position: relative;
+
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.btn {
+  display: block;
+
+  margin: 20px auto;
+  padding: 15px 25px;
+
+  background-color: #e5e5f5;
+  border-radius: 293px;
+  border: none;
+  color: #afafb9;
+
+  cursor: pointer;
+  transition: all .2s;
+  outline: none;
+}
+
+.btn--light { background-color: white; }
+
+.btn--warning {
+  color: #a26a6a;
+  background-color: #f8aaaa;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script>
+  export default {
+  	name: 'task',
+  	data: function(){
+  	  return {
+        label: '',
+        description: '',
+        completed: '',
+  	  }
+  	}
+  }
+</script>
